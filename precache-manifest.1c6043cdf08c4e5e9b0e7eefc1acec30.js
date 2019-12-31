72f243ee7b228cbcebe1ff3a4edb7ca623511701
Art Parkeenvincha <thanut@satang.com>
2019-12-30 22:56:46 -0800

    Updates

---
+++ precache-manifest.1c6043cdf08c4e5e9b0e7eefc1acec30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778857b533d6f18eaf6bea7d1d64753a",
+    "url": "/JojoEyeCatches/index.html"
+  },
+  {
+    "revision": "cd77e335500d96c74c71",
+    "url": "/JojoEyeCatches/static/css/main.9424168a.chunk.css"
+  },
+  {
+    "revision": "5084a02b60999a953eb2",
+    "url": "/JojoEyeCatches/static/js/2.3e01aa45.chunk.js"
+  },
+  {
+    "revision": "cd77e335500d96c74c71",
+    "url": "/JojoEyeCatches/static/js/main.ab940694.chunk.js"
+  },
+  {
+    "revision": "5f7d963f725f22a73bf4",
+    "url": "/JojoEyeCatches/static/js/runtime-main.8ee230c8.js"
+  },
+  {
+    "revision": "44ba4abee028c55770fef4666a550492",
+    "url": "/JojoEyeCatches/static/media/Josuke.44ba4abe.png"
+  },
+  {
+    "revision": "6c5e51d8ea8b5b37ec257583d66c0c8f",
+    "url": "/JojoEyeCatches/static/media/SoftAndWet.6c5e51d8.png"
+  }
+]);
